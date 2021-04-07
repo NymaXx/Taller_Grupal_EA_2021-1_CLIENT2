ca073866025743e32ee4743bdfb9f12c0a59aeac
NymaXx <53951684+NymaXx@users.noreply.github.com>
2021-04-07 16:00:45 -0500

    Start Activity interface(2)

--- app/src/main/java/com/example/taller_grupal_ea_2021_1_client2/MainActivity.java
+++ app/src/main/java/com/example/taller_grupal_ea_2021_1_client2/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.taller_grupal_ea_2021_1_client2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private Button startBtn;
+    private Button toControlsBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startBtn = findViewById(R.id.startBtn);
+        toControlsBtn = findViewById(R.id.toControlsBtn);
+
+        startBtn.setOnClickListener(
+                (v) -> {
+
+                }
+        );
+
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
